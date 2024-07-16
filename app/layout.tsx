7185105414eb29dc276f39063aed5b99b97b4303
Michael Ogbuma <ogbumamichael@gmail.com>
2024-07-16 21:03:45 +0100

    - refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Nav from "./components/Nav";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "StraitPay Todo App by Michael Ogbuma",
+  title: "Todo App by Michael Ogbuma",
   description: "Built by Michael Ogbuma",
 };
 
